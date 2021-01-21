e0bd5d79464c9507159a63c0043c16464ee098ee
Meerim <meerim@Meerims-MacBook-Air.local>
2021-01-21 20:02:03 +0600

    first commit

---
+++ main.py
@@ -0,0 +1,29 @@
+x = 10
+y = 112
+z = x
+x = y
+y = z
+print(x,y)
+
+
+
+number_1 = int(input('Enter your first number: '))
+number_2 = int(input('Enter your second number: '))
+number_3 = int(input('Enter your third number: '))
+number_4 = int(input('Enter your fourth number: '))
+print(number_1 + number_2)
+print(number_3 * number_4)
+print(number_3 / number_1)
+print(number_4 - number_2)
+
+
+
+
+
+
+
+
+
+
+
+
