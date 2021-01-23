60d6f918697ace1da60cd4544868af752beb07c1
Meerim <meerim@Meerims-MacBook-Air.local>
2021-01-23 09:54:45 +0600

    second commit

--- meerim.py
+++ meerim.py
@@ -1,10 +1,14 @@
-a = input("Введите первое число")
-b = input("Введите второе число")
+a = int(input("Введите первое число"))
+b = int(input("Введите второе число"))
 x = input("Введите операцию между числами")
 if x == '+':
     print(a+b)
 elif x == '-':
     print(a-b)
+elif x == '*':
+    print(a*b)
+elif x == '/':
+    print(a/b)
 else:
     print("Повторите еще раз")
 
